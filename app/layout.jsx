70f674a5b242d05eb3b6afe4d85207cf9bd8c9e0
ROHITH SAGAR M <rohithsagar14325@gmail.com>
2025-11-28 23:37:37 +0400

    Refactor layout component and add Google Tag Manager

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,36 +2,61 @@ import { Outfit } from "next/font/google";
 import { Toaster } from "react-hot-toast";
 import StoreProvider from "@/app/StoreProvider";
 import "./globals.css";
-import React from 'react'
+import Script from "next/script";
+import React from "react";
 import MetaPixel from "@/components/MetaPixel";
 
 const outfit = Outfit({ subsets: ["latin"], weight: ["400", "500", "600"] });
 
 export const metadata = {
   title: "Quickfynd - Shop smarter",
-  description: "Discover trending gadgets, fashion, home essentials & more at the best price. Fast delivery, secure checkout, and deals you don’t want to miss.",
+  description:
+    "Discover trending gadgets, fashion, home essentials & more at the best price. Fast delivery, secure checkout, and deals you don’t want to miss.",
 };
 
 export default function RootLayout({ children }) {
   const ik = process.env.IMAGEKIT_URL_ENDPOINT;
   let ikOrigin = null;
-  try { if (ik) ikOrigin = new URL(ik).origin; } catch {}
+  try {
+    if (ik) ikOrigin = new URL(ik).origin;
+  } catch {}
+
   return (
     <html lang="en">
       <head>
+        {/* ImageKit Optimization */}
         {ikOrigin && (
           <>
             <link rel="dns-prefetch" href={ikOrigin} />
             <link rel="preconnect" href={ikOrigin} crossOrigin="anonymous" />
           </>
         )}
-          {/* Meta Pixel Code moved to Client Component */}
-    
-        {/* End Meta Pixel Code */}
 
+        {/* Google Tag Manager - HEAD */}
+        <Script id="gtm-head" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5QLZ2255');
+          `}
+        </Script>
       </head>
+
       <body className={`${outfit.className} antialiased`} suppressHydrationWarning>
-          <MetaPixel />
+        <MetaPixel />
+
+        {/* Google Tag Manager (noscript required for browsers with JS disabled) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5QLZ2255"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
         <StoreProvider>
           <Toaster />
           {children}
